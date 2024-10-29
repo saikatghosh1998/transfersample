16d95c4984f22d0c3d1a3ef85c719447201fad9a
Saikat Ghosh <35963374+saikatghosh1998@users.noreply.github.com>
2024-10-29 11:54:53 +0530

    Update codebuild.py

--- codebuild.py
+++ codebuild.py
@@ -3,43 +3,52 @@ import json
 
 def list_codebuild_projects():
     client = boto3.client('codebuild')
-    projects_info = []
-    
-    # List all CodeBuild project names
-    project_names = client.list_projects()['projects']
-    
-    for project_name in project_names:
-        # Fetch detailed information for each project
-        project_info = client.batch_get_projects(names=[project_name])['projects'][0]
-        
-        # Extract relevant details
-        environment_variables = project_info['environment'].get('environmentVariables', [])
-        
-        project_details = {
-            'ProjectName': project_info['name'],
-            'ServiceRole': project_info.get('serviceRole', 'N/A'),
-            'EnvironmentVariables': [
-                {
-                    'Name': var.get('name'),
-                    'Value': var.get('value'),
-                    'Type': var.get('type', 'PLAINTEXT')
-                } for var in environment_variables
-            ]
-        }
-        
-        projects_info.append(project_details)
-    
-    return projects_info
+    projects_data = []
 
-def write_to_file(projects_info, file_name="codebuild_projects.json"):
+    # Retrieve the list of CodeBuild project names
+    project_names = client.list_projects()['projects']
+
+    for project_name in project_names:
+        # Get project details
+        project_details = client.batch_get_projects(names=[project_name])['projects'][0]
+        
+        # Extract environment variables
+        environment_variables = [
+            {
+                "Name": env_var['name'],
+                "Value": env_var['value'],
+                "Type": env_var.get('type', 'PLAINTEXT')  # Default type to PLAINTEXT if not specified
+            }
+            for env_var in project_details['environment']['environmentVariables']
+        ]
+        
+        # Get buildspec (if specified in the project configuration)
+        buildspec = project_details.get('source', {}).get('buildspec', 'Not specified in project')
+
+        # Structure project data
+        project_info = {
+            'ProjectName': project_name,
+            'EnvironmentVariables': environment_variables,
+            'Buildspec': buildspec,
+            'ServiceRole': project_details.get('serviceRole', 'N/A'),
+            'EnvironmentType': project_details['environment']['type'],
+            'Image': project_details['environment'].get('image', 'N/A'),
+            'ComputeType': project_details['environment'].get('computeType', 'N/A')
+        }
+
+        projects_data.append(project_info)
+    
+    return projects_data
+
+def write_to_file(projects_data, file_name="codebuild_projects.json"):
     with open(file_name, 'w') as f:
-        json.dump(projects_info, f, indent=4)
+        json.dump(projects_data, f, indent=4)
     
     print(f"Data has been written to {file_name}")
 
 def main():
-    projects_info = list_codebuild_projects()
-    write_to_file(projects_info)
+    projects_data = list_codebuild_projects()
+    write_to_file(projects_data)
 
 if __name__ == "__main__":
     main()
